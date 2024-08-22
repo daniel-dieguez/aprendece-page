643d6a65e09a7e641d617ff31b2b5e77ecf8059d
daniel-dieguez <danguez2001@gmail.com^C>
2024-08-22 14:14:15 -0600

    1.1 commite del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from '../src/style/inicio.module.css'
+
+export default function App() {
+  return (
+    <div className={Style.cuerpo}>
+        
+    </div>
+  )
+}
